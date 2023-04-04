a2f467ece9c616e4d653f4217cf24a09a66bfa9a
Andre <phandoananh1609@gmail.com>
2023-04-04 22:22:59 +0700

    xong r nèk

--- script.js
+++ script.js
@@ -18,8 +18,6 @@ function startVideo() {
       const resizedDetections = faceapi.resizeResults(detections, displaySize);
       canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
       faceapi.draw.drawDetections(canvas, resizedDetections);
-      faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
-      faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
     }, 100);
   });
 }
